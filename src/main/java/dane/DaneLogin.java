4eb2dafe623cc984bb595382aeb09985d9c088c2
gorzbi <zbigniewgornowicz@wp.pl>
2025-04-09 09:14:10 +0200

    Utworzenie paczki Dane

---
+++ src/main/java/dane/DaneLogin.java
@@ -0,0 +1,15 @@
+package dane;
+
+public class DaneLogin {
+
+    String username = "elo";
+    String password = "elo";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
